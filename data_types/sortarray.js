740ba51ee6808b24235a22e46760a9fcb150912f
suhas7527 <suhasbk249@gmail.com>
2021-11-02 22:47:48 -0700

    basic node js code

---
+++ data_types/sortarray.js
@@ -0,0 +1,3 @@
+var num = [4, 1, 6, -2, 2, -5, 8];
+num.sort();
+console.log(num);
---
+++ data_types/studentDist.js
@@ -0,0 +1,6 @@
+var Student = {
+  FirstName: "Suhas Khandappagoudar",
+  Usn: "01FE20MCA029",
+  Department: "MCA",
+};
+console.log(Object.keys(Student));
